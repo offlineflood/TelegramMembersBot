e3f93ca5d2ecf7e6ba63b615dc947fa0a3ff939a
Aliyev Ali <108521240+offlineflood@users.noreply.github.com>
2023-04-06 16:33:41 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-#!/bin/env python3
-# code by : youtube.com/theunknon
+# @DegGixM & @DejavuTeam & @DejavuSupport.
+
 
 """
 
-you can re run setup.py 
-if you have added some wrong value
+setup.py-ni yenidən işə sala bilərsiniz
+bəzi səhv dəyər əlavə etmisinizsə
 
 """
 re="\033[1;31m"
@@ -27,24 +27,24 @@ def requirements():
 		banner()
 		print(gr+'['+cy+'+'+gr+']'+cy+' this may take some time ...')
 		os.system("""
-			pip3 install cython numpy pandas
-			python3 -m pip install cython numpy pandas
+                          pip cython numpy pandaları quraşdırın
+                          python -m pip quraşdırma cython numpy pandas
 			""")
 	banner()
-	print(gr+'['+cy+'+'+gr+']'+cy+' it will take upto 10 min to install csv merge.')
-	input_csv = input(gr+'['+cy+'+'+gr+']'+cy+' do you want to enable csv merge (y/n): ').lower()
+	print(gr+'['+cy+'+'+gr+']'+cy+' csv birləşməsini quraşdırmaq 10 dəqiqə çəkəcək.')
+	input_csv = input(gr+'['+cy+'+'+gr+']'+cy+' csv birləşməsini aktiv etmək istəyirsiniz (y/n): ').lower()
 	if input_csv == "y":
 		csv_lib()
 	else:
 		pass
-	print(gr+"[+] Installing requierments ...")
+	print(gr+"[+] Quraşdırma tələbləri ...")
 	os.system("""
-		pip3 install telethon requests configparser
-		python3 -m pip install telethon requests configparser
-		touch config.data
+		pip konfiqurasiya analizatorunu quraşdırın
+                python -m pip quraşdırma teleton sorğuları konfiqurasiya analizatoru
+                config.data-ya toxunun
 		""")
 	banner()
-	print(gr+"[+] requierments Installed.\n")
+	print(gr+"[+] tələblər quraşdırılıb.\n")
 
 
 def config_setup():
@@ -52,16 +52,16 @@ def config_setup():
 	banner()
 	cpass = configparser.RawConfigParser()
 	cpass.add_section('cred')
-	xid = input(gr+"[+] enter api ID : "+re)
+	xid = input(gr+"[+] API ID daxil edin : "+re)
 	cpass.set('cred', 'id', xid)
-	xhash = input(gr+"[+] enter hash ID : "+re)
+	xhash = input(gr+"[+] HASH ID daxil edin : "+re)
 	cpass.set('cred', 'hash', xhash)
-	xphone = input(gr+"[+] enter phone number : "+re)
+	xphone = input(gr+"[+] telefon nömrəsini daxil edin : "+re)
 	cpass.set('cred', 'phone', xphone)
 	setup = open('config.data', 'w')
 	cpass.write(setup)
 	setup.close()
-	print(gr+"[+] setup complete !")
+	print(gr+"[+] quraşdırma tamamlandı !")
 
 def merge_csv():
 	import pandas as pd
@@ -69,59 +69,59 @@ def merge_csv():
 	banner()
 	file1 = pd.read_csv(sys.argv[2])
 	file2 = pd.read_csv(sys.argv[3])
-	print(gr+'['+cy+'+'+gr+']'+cy+' merging '+sys.argv[2]+' & '+sys.argv[3]+' ...')
-	print(gr+'['+cy+'+'+gr+']'+cy+' big files can take some time ... ')
+	print(gr+'['+cy+'+'+gr+']'+cy+' birləşmə '+sys.argv[2]+' & '+sys.argv[3]+' ...')
+	print(gr+'['+cy+'+'+gr+']'+cy+' böyük fayllar bir az vaxt ala bilər ... ')
 	merge = file1.merge(file2, on='username')
 	merge.to_csv("output.csv", index=False)
-	print(gr+'['+cy+'+'+gr+']'+cy+' saved file as "output.csv"\n')
+	print(gr+'['+cy+'+'+gr+']'+cy+' faylı "output.csv" kimi saxladı\n')
 
 def update_tool():
 	import requests as r
 	banner()
-	source = r.get("https://raw.githubusercontent.com/th3unkn0n/TeleGram-Scraper/master/.image/.version")
+	source = r.get("https://raw.githubusercontent.com/offlineflood/TelegramMembersBot/master/.image/.version")
 	if source.text == '3':
-		print(gr+'['+cy+'+'+gr+']'+cy+' alredy latest version')
+		print(gr+'['+cy+'+'+gr+']'+cy+' artıq son versiya')
 	else:
-		print(gr+'['+cy+'+'+gr+']'+cy+' removing old files ...')
+		print(gr+'['+cy+'+'+gr+']'+cy+' köhnə faylları silmək ...')
 		os.system('rm *.py');time.sleep(3)
-		print(gr+'['+cy+'+'+gr+']'+cy+' getting latest files ...')
+		print(gr+'['+cy+'+'+gr+']'+cy+' ən son faylları əldə etmək ...')
 		os.system("""
-			curl -s -O https://raw.githubusercontent.com/th3unkn0n/TeleGram-Scraper/master/add2group.py
-			curl -s -O https://raw.githubusercontent.com/th3unkn0n/TeleGram-Scraper/master/scraper.py
-			curl -s -O https://raw.githubusercontent.com/th3unkn0n/TeleGram-Scraper/master/setup.py
-			curl -s -O https://raw.githubusercontent.com/th3unkn0n/TeleGram-Scraper/master/smsbot.py
+			curl -s -O https://raw.githubusercontent.com/offlineflood/TelegramMembersBot/master/add2group.py
+			curl -s -O https://raw.githubusercontent.com/offlineflood/TelegramMembersBot/master/scraper.py
+			curl -s -O https://raw.githubusercontent.com/offlineflood/TelegramMembersBot/master/setup.py
+			curl -s -O https://raw.githubusercontent.com/offlineflood/TelegramMembersBot/master/smsbot.py
 			chmod 777 *.py
 			""");time.sleep(3)
-		print(gr+'\n['+cy+'+'+gr+']'+cy+' update compled.\n')
+		print(gr+'\n['+cy+'+'+gr+']'+cy+' yeniləmə tamamlandı.\n')
 
 try:
 	if any ([sys.argv[1] == '--config', sys.argv[1] == '-c']):
-		print(gr+'['+cy+'+'+gr+']'+cy+' selected module : '+re+sys.argv[1])
+		print(gr+'['+cy+'+'+gr+']'+cy+' seçilmiş modul : '+re+sys.argv[1])
 		config_setup()
 	elif any ([sys.argv[1] == '--merge', sys.argv[1] == '-m']):
-		print(gr+'['+cy+'+'+gr+']'+cy+' selected module : '+re+sys.argv[1])
+		print(gr+'['+cy+'+'+gr+']'+cy+' seçilmiş modul : '+re+sys.argv[1])
 		merge_csv()
 	elif any ([sys.argv[1] == '--update', sys.argv[1] == '-u']):
-		print(gr+'['+cy+'+'+gr+']'+cy+' selected module : '+re+sys.argv[1])
+		print(gr+'['+cy+'+'+gr+']'+cy+' seçilmiş modul : '+re+sys.argv[1])
 		update_tool()
 	elif any ([sys.argv[1] == '--install', sys.argv[1] == '-i']):
 		requirements()
 	elif any ([sys.argv[1] == '--help', sys.argv[1] == '-h']):
 		banner()
-		print("""$ python3 setup.py -m file1.csv file2.csv
+		print("""$ python setup.py -m file1.csv file2.csv
 			
-	( --config  / -c ) setup api configration
-	( --merge   / -m ) merge 2 .csv files in one 
-	( --update  / -u ) update tool to latest version
-	( --install / -i ) install requirements
-	( --help    / -h ) show this msg 
+	( --config  / -c ) api konfiqurasiyasını quraşdırın
+	( --merge   / -m ) 2 .csv faylını birində birləşdirin 
+	( --update  / -u ) aləti ən son versiyaya yeniləyin
+	( --install / -i ) quraşdırma tələbləri
+	( --help    / -h ) bu mesajı göstər 
 			""")
 	else:
-		print('\n'+gr+'['+re+'!'+gr+']'+cy+' unknown argument : '+ sys.argv[1])
-		print(gr+'['+re+'!'+gr+']'+cy+' for help use : ')
-		print(gr+'$ python3 setup.py -h'+'\n')
+		print('\n'+gr+'['+re+'!'+gr+']'+cy+' naməlum arqument : '+ sys.argv[1])
+		print(gr+'['+re+'!'+gr+']'+cy+' kömək istifadə üçün : ')
+		print(gr+'$ python setup.py -h'+'\n')
 except IndexError:
-	print('\n'+gr+'['+re+'!'+gr+']'+cy+' no argument given : '+ sys.argv[1])
-	print(gr+'['+re+'!'+gr+']'+cy+' for help use : ')
-	print(gr+'['+re+'!'+gr+']'+cy+' https://github.com/th3unkn0n/TeleGram-Scraper#-how-to-install-and-use')
-	print(gr+'$ python3 setup.py -h'+'\n')
+	print('\n'+gr+'['+re+'!'+gr+']'+cy+' heç bir arqument verilməmişdir : '+ sys.argv[1])
+	print(gr+'['+re+'!'+gr+']'+cy+' kömək istifadə üçün : ')
+	print(gr+'['+re+'!'+gr+']'+cy+' https://github.com/offlineflood/TelegramMembersBot#-how-to-install-and-use')
+	print(gr+'$ python setup.py -h'+'\n')
